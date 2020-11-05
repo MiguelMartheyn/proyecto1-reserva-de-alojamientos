d2ff2e99dba733cbc38962996317a2308c9f2d32
Miguel Martheyn <MiguelMartheyn@hotmail.com>
2020-11-05 11:59:18 -0500

    Updates

---
+++ precache-manifest.06dd404262f76a7e62a6eef618613145.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d6b0360e7a6d05e538eb6f5d74f6cc2",
+    "url": "/proyecto1-reserva-de-alojamientos/index.html"
+  },
+  {
+    "revision": "bc557732307986d6426d",
+    "url": "/proyecto1-reserva-de-alojamientos/static/css/main.01007143.chunk.css"
+  },
+  {
+    "revision": "69c2242d59c17d9cef81",
+    "url": "/proyecto1-reserva-de-alojamientos/static/js/2.4037962f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/proyecto1-reserva-de-alojamientos/static/js/2.4037962f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc557732307986d6426d",
+    "url": "/proyecto1-reserva-de-alojamientos/static/js/main.eb008d45.chunk.js"
+  },
+  {
+    "revision": "01d3764819bcb42a782b",
+    "url": "/proyecto1-reserva-de-alojamientos/static/js/runtime-main.f4489289.js"
+  },
+  {
+    "revision": "632f5b254ee2c289c3dbf9bd06aab30a",
+    "url": "/proyecto1-reserva-de-alojamientos/static/media/alto-atacama.632f5b25.jpg"
+  },
+  {
+    "revision": "7c8f1bd221cdc809ede7c80d286fca90",
+    "url": "/proyecto1-reserva-de-alojamientos/static/media/azur-real.7c8f1bd2.jpg"
+  },
+  {
+    "revision": "ee9a61355483d2b02ed1791f855112e6",
+    "url": "/proyecto1-reserva-de-alojamientos/static/media/campo-tinto.ee9a6135.jpg"
+  },
+  {
+    "revision": "b1c377ccfafe0ec2af608533de1e038e",
+    "url": "/proyecto1-reserva-de-alojamientos/static/media/casa-higueras.b1c377cc.jpg"
+  },
+  {
+    "revision": "f91c2e01e1f020aaa67e1dc18fb6c2da",
+    "url": "/proyecto1-reserva-de-alojamientos/static/media/casa-turquesa.f91c2e01.jpg"
+  },
+  {
+    "revision": "63abb3f623eef33bbbba914b3ebc63d9",
+    "url": "/proyecto1-reserva-de-alojamientos/static/media/entre-cielos.63abb3f6.jpg"
+  },
+  {
+    "revision": "40ad2d8d202c2e84f5ed9fa78329df9f",
+    "url": "/proyecto1-reserva-de-alojamientos/static/media/habitacion.40ad2d8d.png"
+  },
+  {
+    "revision": "ea003688e1120c772fbbc82502143892",
+    "url": "/proyecto1-reserva-de-alojamientos/static/media/huacalera.ea003688.jpg"
+  },
+  {
+    "revision": "07731ebafd4ff3f841a7813d5590790e",
+    "url": "/proyecto1-reserva-de-alojamientos/static/media/la-bamba-de-areco.07731eba.jpg"
+  },
+  {
+    "revision": "5f33c37723ee86e523144fe75b7f333f",
+    "url": "/proyecto1-reserva-de-alojamientos/static/media/luma-casa-de-montana.5f33c377.jpg"
+  },
+  {
+    "revision": "4fba0fc1a00bd1f44d4bbe838d131519",
+    "url": "/proyecto1-reserva-de-alojamientos/static/media/merced-del-alto.4fba0fc1.jpg"
+  },
+  {
+    "revision": "edf738832d2ccc456ed832101839f4a9",
+    "url": "/proyecto1-reserva-de-alojamientos/static/media/mundo2.edf73883.png"
+  },
+  {
+    "revision": "100bce15f19c89a9e03c086d5a98575d",
+    "url": "/proyecto1-reserva-de-alojamientos/static/media/peso.100bce15.png"
+  },
+  {
+    "revision": "43e1212af379a730eedc2421b5d692a4",
+    "url": "/proyecto1-reserva-de-alojamientos/static/media/plantilla - banner_Mesa de trabajo 1.43e1212a.jpg"
+  },
+  {
+    "revision": "f24a2410302a08004c870677c6c3add4",
+    "url": "/proyecto1-reserva-de-alojamientos/static/media/ponta-dos-ganchos.f24a2410.jpg"
+  },
+  {
+    "revision": "e8d6823f195650eebc96a78a5a868009",
+    "url": "/proyecto1-reserva-de-alojamientos/static/media/rincon-del-socorro.e8d6823f.jpg"
+  },
+  {
+    "revision": "472d43c7782e8b8312a81e08f56428b5",
+    "url": "/proyecto1-reserva-de-alojamientos/static/media/sainte-jeanne.472d43c7.jpg"
+  },
+  {
+    "revision": "7a5770914da6ee1452a98b49492228b0",
+    "url": "/proyecto1-reserva-de-alojamientos/static/media/tierra-patagonia.7a577091.jpg"
+  },
+  {
+    "revision": "a77b33b254e9fe91f3b24c1878cd5e67",
+    "url": "/proyecto1-reserva-de-alojamientos/static/media/uxua-casa.a77b33b2.jpg"
+  },
+  {
+    "revision": "e578428a8ae3fb6da2513dfaf1a163bf",
+    "url": "/proyecto1-reserva-de-alojamientos/static/media/vik-chile.e578428a.jpg"
+  },
+  {
+    "revision": "cb67834d6d866e9f781af5ccc98b1922",
+    "url": "/proyecto1-reserva-de-alojamientos/static/media/vila-da-santa.cb67834d.jpg"
+  },
+  {
+    "revision": "22300bcf4188d82e2376e7f3c31ff898",
+    "url": "/proyecto1-reserva-de-alojamientos/static/media/vira-vira.22300bcf.jpg"
+  }
+]);
